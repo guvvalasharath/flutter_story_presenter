d851ce651da766fab7fb1e1bf859a74c4bae000a
guvvalasharath <sharath.kumar@numecrypto.com>
2025-11-24 19:22:28 +0530

    feat: dependency updates

--- lib/src/story_presenter/story_view.dart
+++ lib/src/story_presenter/story_view.dart
@@ -237,7 +237,7 @@ class _FlutterStoryPresenterState extends State<FlutterStoryPresenter>
     }
 
     if (currentItem.audioConfig != null) {
-      _audioPlayer?.durationFuture?.then((v) {
+      _audioPlayer?.durationStream.first.then((v) {
         _totalAudioDuration = v;
         _animationController ??= AnimationController(
           vsync: this,
